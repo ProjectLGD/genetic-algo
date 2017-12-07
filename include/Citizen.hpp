ae8c9c642e11fa7693997f4cfcdcce53b0d98025
Yorick Rommers <yjpmrommers@gmail.com>
2017-12-07 15:53:09 +0100

    Added citizen, implemented citizen in population

---
+++ include/Citizen.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Citizen {
+public:
+    DNA<T> dna;
+
+    float fitness = 0;
+
+    Citizen(DNA<T> dna) {
+        this->dna = dna;
+    }
+
+    virtual float fitness_calculate(T target) {
+        cerr << "Calculting fitness in citizen" << endl;
+        (void) target;
+        return 0.0f;
+    }
+
+    virtual float fitness_normalize(float max) {
+        fitness /= max;
+        return fitness;
+    }
+
+    virtual void run(T target) {
+        cerr << "Running citizen, are you sure this is supposed to happen?" << endl;
+        (void) target;
+    }
+
+};
--- include/Population.hpp
+++ include/Population.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
 #include <iostream>
+#include "DNA.hpp"
+#include "Citizen.hpp"
 
 using namespace std;
 
-template <class T>
+template <class T, class R>
 class Population {
-    typedef vector<DNA<T>>(*dna_gen_func)(uint64_t vector_size, uint64_t dna_size);
+    typedef vector<R>(*dna_gen_func)(uint64_t vector_size, uint64_t dna_size);
 private:
     T target; // our target, T. The population wants to be as close as possible to target.
     float mutation_rate; // how often mutations occur.
     uint64_t population_size; // The size of the
-    vector<DNA<T>> popu; // current population filled with DNA
-    vector<DNA<T>> mating_pool; // the pool used when making new generation
+    vector<R> popu; // current population filled with DNA
+    vector<R> mating_pool; // the pool used when making new generation
     bool finished = false; // not in use yet
     int perfect_score = 1; // not in use yet.
     uint64_t generation = 0; // current generation
@@ -29,16 +31,16 @@ public:
         // fill population with DNA.
         // TODO: remove hardcoded value;
         // Generate random DNA, so we're generating 100 DNA<T>'s of size 100;
-        vector<DNA<T>> random_dna = dna_generate(population_size, dna_size);
+        vector<R> random_dna = dna_generate(population_size, dna_size);
         for (size_t i = 0; i < random_dna.size(); i++) {
-            DNA<T> dna = random_dna.at(i);
+            R dna = random_dna.at(i);
             popu.push_back(dna); // this copies the dna to popu
         }
 
-        fitness_calculate();
+        // fitness_calculate();
     }
 
-    Population(const Population<T> *p) {
+    Population(const Population<T, R> *p) {
         this->target = p->target;
         this->mutation_rate = p->mutation_rate;
         this->population_size = p->population_size;
@@ -50,9 +52,13 @@ public:
     void fitness_calculate() {
         for (size_t i = 0; i < popu.size(); i++) {
             // These have to be pointers otherwise we're not accesing the actual DNA inside the vec.
-            DNA<T> *dna = &popu.at(i);
+            R *citizen = &popu.at(i);
+            citizen->run(target);
             // cout << "Dna fitness is " << dna->fitness_get();
-            dna->fitness_calculate(target);
+            // dna->fitness_calculate(target);
+            citizen->fitness_calculate(target);
+            // cout << "Fitness is " << endl;
+            // cout << citizen->fitness << endl;
             // cout << " and now it is " << dna->fitness_get() << endl;
         }
     }
@@ -63,17 +69,17 @@ public:
         // Find max fitness.
         float fitness_max = 0.0f;
         for (size_t i = 0; i < popu.size(); i++) {
-            DNA<T> *dna = &popu.at(i);
+            R *cit = &popu.at(i);
             // cout << "Dna fitness " << dna->fitness_get() << endl;
-            if (dna->fitness_get() > fitness_max) {
-                fitness_max = dna->fitness_get();
+            if (cit->fitness > fitness_max) {
+                fitness_max = cit->fitness;
             }
         }
 
         // cout << "Fitness max is " << fitness_max << endl;
 
         for (size_t i = 0; i < popu.size(); i++) {
-            DNA<T> *dna = &popu.at(i);
+            R *cit = &popu.at(i);
 
             // So, what we're going to do next is, find the most fit DNA's fitness.
             // If the DNA is more fit, it should be added to our mating pool more often.
@@ -82,12 +88,12 @@ public:
             // So for example, a piece of DNA with a fitness of 1 should be added 10 times.
             // A piece of DNA with a fitness of 0.1 should be added only one time.
             unsigned int count = 0;
-            dna->fitness_normalize(fitness_max);
-            float fitness_current = dna->fitness_get();
+            cit->fitness_normalize(fitness_max);
+            float fitness_current = cit->fitness;
             count = (unsigned int)(10 * fitness_current);
             // cout << "Adding fitness of " << fitness_current << "\t" << count << " times to mating_pool" << endl;
             for (size_t j = 0; j < count; j++) {
-                mating_pool.push_back(*dna);
+                mating_pool.push_back(*cit);
             }
         }
         // cout << "Mating pool size: " << mating_pool.size() << endl;
@@ -106,8 +112,8 @@ public:
 
         for (size_t i = 0; i < popu.size(); i++) {
             // Get parents
-            DNA<T> partner1 = mating_pool.at(rand() % (mating_pool.size() - 1));
-            DNA<T> partner2 = mating_pool.at(rand() % (mating_pool.size() - 1));
+            DNA<T> partner1 = mating_pool.at(rand() % (mating_pool.size() - 1)).dna;
+            DNA<T> partner2 = mating_pool.at(rand() % (mating_pool.size() - 1)).dna;
 
             // Make new child
             DNA<T> child = partner1.crossover(&partner2);
@@ -117,25 +123,26 @@ public:
             // partner2.print();
             // child.print();
 
-            vector<T> random_values = dna_generate(1, child.gene_size()).at(0).gene_get();
+            vector<T> random_values = dna_generate(1, child.gene_size()).at(0).dna.gene_get();
             child.mutate(mutation_rate, random_values);
 
-            // add child to population.
-            popu.at(i) = child;
+            // add child to population as new citizen.
+            // TODO: Check if we shouldn't pas pos
+            popu.at(i) = R(child);
         }
-        fitness_calculate(); // don't forget to calculate fitness so that all our new children are fit =D
+        // fitness_calculate(); // don't forget to calculate fitness so that all our new children are fit =D
         // If they're the same, that's not good.
         // popu.at(0).print();
         generation++;
     }
 
     DNA<T> compute_most_fit() {
-        fitness_calculate();
+        // fitness_calculate();
         float fitness_max = 0.0f;
         size_t index = 0;
         for (size_t i = 0; i < popu.size(); i++) {
-            DNA<T> dna = popu.at(i);
-            float fitness_current = dna.fitness_get();
+            R *cit = &popu.at(i);
+            float fitness_current = cit->fitness;
             // cout << "Current fitness " << fitness_current << endl;
             if (fitness_current > fitness_max) {
                 fitness_max = fitness_current;
@@ -143,15 +150,24 @@ public:
             }
         }
         cout << "The most fit DNA is index " << index << " with a fitness of " << fitness_max << endl;
-        return popu.at(index);
+        cout << "Pos is " << popu.at(index) << endl;
+        cout << "Distance is " << popu.at(index).pos.get_distance(target) << endl;
+        return popu.at(index).dna;
     }
 
-    vector<DNA<T>> dna_generate(uint64_t vector_amount, uint64_t dna_amount) {
+    vector<R> dna_generate(uint64_t vector_amount, uint64_t dna_amount) {
         return gen_func(vector_amount, dna_amount);
     }
 
-    vector<DNA<T>> get_dna() {
+    vector<R> get_dna() {
         return popu;
+    }
+
+    void run() {
+        for (size_t i = 0; i < popu.size(); i++) {
+            // for citz
+            popu.at(i).run(target);
+        }
     }
 
     void print() {
@@ -164,7 +180,7 @@ public:
         cout << endl;
     }
 
-    bool operator==(const Population<T> &other) const {
+    bool operator==(const Population<T, R> &other) const {
         return (this->generation == other.generation) && (this->popu == other.popu);
     }
 };
